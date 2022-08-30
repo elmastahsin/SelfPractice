1ad79d4ac041e9f1d615d24e5b5b90f8f189ce86
Tyson <tyson@cydeo.com>
2022-08-30 13:08:28 +0300

    Initial commit

--- src/day11_string/TipCalculator.java
+++ src/day11_string/TipCalculator.java
@@ -1,14 +1,44 @@
 package day11_string;
 
+import java.util.Scanner;
+
 public class TipCalculator {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Split or No split (Yes or No):");
+        String yesN0 = input.next().toLowerCase();
+
+        System.out.println("Enter the number of people (number): ");
+        int numOfPeople = input.nextInt();
+
+        System.out.println("Enter the check amount (number): ");
+        double checkAmount = input.nextDouble();
+
+        System.out.println("How was the Service Quality (Poor = 5%, Fair = 10%, " +
+                "Good = 15%, Great = 20%, Excellent = 25%):");
+        String serviceQuality = input.next().toLowerCase();
+
+        input.close();
+
+        double totalTip = (serviceQuality.equals("excellent")) ? checkAmount * 0.25 : (serviceQuality.equals("great")) ? checkAmount * 0.2 : (serviceQuality.equals("good")) ? checkAmount * 0.15 : (serviceQuality.equals("fair")) ? checkAmount * 0.1 : checkAmount * 0.5;
+
+        double totalToPay = checkAmount + totalTip;
+
+        System.out.println("Number of People: " + numOfPeople);
+        System.out.println("Total to pay: " + totalToPay);
+        System.out.println("Total tip: " + totalTip);
+        if (yesN0.equals("yes")) {
+            System.out.println("Total per person: " + totalToPay / numOfPeople);
+            System.out.println("Tip per person: " + totalTip / numOfPeople);
+        }
     }
 }
 /*
 * 10. Create a class called TipCalculator, and write a program for a tip calculator.
     There will be different service quality benchmarks that will determine the tip given.
-    There will also the ability to calculate based on the number of people in the party and if there is a split of the bill or not.
+    There will also the ability to calculate based on the number of people in the party a
+    * and if there is a split of the bill or not.
 
         Poor = 5%, Fair = 10%, Good = 15%, Great = 20%, Excellent = 25%
 
