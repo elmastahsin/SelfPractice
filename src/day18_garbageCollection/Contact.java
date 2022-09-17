8280b9c073ae3fffecb4166ce750edc0bef1505b
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-09-17 11:25:19 +0300

    Initial commit

--- src/day18_garbageCollection/Contact.java
+++ src/day18_garbageCollection/Contact.java
@@ -29,3 +29,10 @@ public class Contact {
                 '}';
     }
 }
+/*1. Write a program that can return the character that has the highest frequency from a string
+	Ex:
+		str = "aaabbccccddeeee"
+
+		output:
+			c
+			e*/
--- src/day18_garbageCollection/HighestFrequency.java
+++ src/day18_garbageCollection/HighestFrequency.java
@@ -50,4 +50,10 @@ public class HighestFrequency {
 
 
 }
+/*1. Write a program that can return the character that has the highest frequency from a string
+	Ex:
+		str = "aaabbccccddeeee"
 
+		output:
+			c
+			e*/
