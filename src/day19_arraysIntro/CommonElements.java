369d4d754953edb9ad06c6ad5520ea2b39ccb1ab
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-09-18 16:51:27 +0300

    Initial commit

--- src/day19_arraysIntro/CommonElements.java
+++ src/day19_arraysIntro/CommonElements.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 
 public class CommonElements {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5},
-                arr2 = {4, 5, 6, 7, 8};
-int [] result = new int[arr1.length];
+        int[] arr1 = {1, 2, 3, 4, 5}, arr2 = {4, 5, 6, 7, 8};
+        int[] result = new int[arr1.length];
 
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] ==  arr2[j]){
+                if (arr1[i] == arr2[j]) {
                     System.out.println(arr1[i]);
                 }
             }
