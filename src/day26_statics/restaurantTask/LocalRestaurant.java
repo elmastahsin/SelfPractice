92092116c6664fbebc0233b93d72494e0bfc4f1d
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-09 11:48:35 +0300

    Constructor Exercises

---
+++ src/day26_statics/restaurantTask/LocalRestaurant.java
@@ -0,0 +1,11 @@
+package day26_statics.restaurantTask;
+
+public class LocalRestaurant {
+}
+/*	4.4. Create a class LocalRestaurant that has a main method with the following:
+	        	- Make a Restaurant object
+	        	- Create an array of servers with their information set. Add those initial servers to the ArrayList of Servers in the Restaurant object
+	            - Create an array of chefs with their information set. Add those initial chefs to the ArrayList of Chefs in the Restaurant object
+
+	            - Print your whole restaurants information
+	            */
