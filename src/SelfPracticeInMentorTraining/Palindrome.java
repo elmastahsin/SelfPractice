7c5ca7ac2d194a2018d9286a1de35627ae4596e5
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-11-09 22:06:10 +0300

    Exercises

---
+++ src/SelfPracticeInMentorTraining/Palindrome.java
@@ -0,0 +1,42 @@
+package SelfPracticeInMentorTraining;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        int reverseNumber = 0;
+        int remainder = 0;
+        int tempNumber = number;
+        while (tempNumber > 0) {
+            remainder = tempNumber % 10;
+            reverseNumber = reverseNumber * 10 + remainder;
+            tempNumber = tempNumber / 10;
+        }
+        if (number == reverseNumber) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        /*
+        String str = input.nextLine();
+
+        String result = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+
+        boolean isPalindrome = str.equalsIgnoreCase(result);
+
+        System.out.println(isPalindrome);
+*/
+
+    }
+}
+/*Problem-2 🡪 Read a number from the user
+ and check if it is palindrome or not
+ (example1 --> input=1234 => output=false, example2 --> input=1221 => output=true)*/
---
+++ src/SelfPracticeInMentorTraining/SortNumbers.java
@@ -0,0 +1,59 @@
+package SelfPracticeInMentorTraining;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+       int a, b, c;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Three Numbers: ");
+
+       /* int [] array = new int[3];
+        for (int i = 0; i < array.length; i++) {
+            array[i] =input.nextInt();
+        }
+        Arrays.sort(array);
+
+        for (int i = array.length-1; i >= 0 ; i--) {
+            System.out.println(array[i]);
+        }*/
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println(a); // a is biggest
+            if (c > b) {
+                System.out.println(c);
+                System.out.println(b);
+            } else {
+                System.out.println(b);
+                System.out.println(c);
+            }
+        } else if (b > a && b > c) { // b is biggest
+            System.out.println(b);
+            if (c > a) {
+                System.out.println(c);
+                System.out.println(a);
+            } else {
+                System.out.println(a);
+                System.out.println(c);
+            }
+        } else if (c > a && c > b) { // c is biggest
+            System.out.println(c);
+            if (b > a) {
+                System.out.println(b);
+                System.out.println(a);
+            } else {
+                System.out.println(a);
+                System.out.println(b);
+            }
+        }
+    }
+}
+/*
+Problem-1 🡪 Read 3 numbers from the user
+and print them like the example in descending
+order.
+(example --> input x=5, y=4, z=6 => "output: 6-5-4")*/
