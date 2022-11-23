663f0c2a06c8fb8dcaad0b3cebabb6a317a14557
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-11-23 19:47:01 +0300

    Exercises

---
+++ src/SelfPracticeInMentorTraining/NonRepeat.java
@@ -0,0 +1,55 @@
+package SelfPracticeInMentorTraining;
+
+import java.util.Scanner;
+
+public class NonRepeat {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an string :");
+
+        String str = input.next().toLowerCase();
+        int indexOf = nonRepeating(str);
+        if (indexOf>-1) {
+            System.out.println("The index of non repeating vovel is :  " + indexOf + " The letter is " + str.charAt(indexOf));
+        }else {
+            System.err.println(indexOf);
+        }
+    }
+
+    public static int nonRepeating(String str) {
+
+
+        for (int i = 0; i < str.length(); i++) {
+
+            boolean uniqueLetter = false;
+            if (isVovel(str.charAt(i))) {
+                uniqueLetter = true;
+                for (int j = 0; j < str.length(); j++) {
+                    if (i != j && (str.charAt(i) == str.charAt(j))) {
+                        uniqueLetter = false;
+                        break;
+                    }
+                }
+            }
+            if (uniqueLetter) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean isVovel(char i) {
+        if (i == 'a' || i == 'e' || i == 'i' || i== 'o' || i == 'u'){
+            return true;
+        }
+        return false;
+    }
+}
+/*Practice Question:
+Given a string, write a method to find the
+first non-repeating vowel (a, e, i, o, u)
+in it and return its index. If it doesn't exist, return -1.
+Assume that all the characters of the String is lowercase.
+(Do not use String class functions other than charAt())
+
+*/
---
+++ src/SelfPracticeInMentorTraining/Test.java
@@ -0,0 +1,32 @@
+package SelfPracticeInMentorTraining;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+        
+
+
+
+    /*    String str = "Guess me";
+        for (int i = 0 ; i<str.length();i++){
+            System.out.println(str.substring(i,i+1));
+        }
+        String [] ar = new String[10];
+        System.out.println(multiply(5.5,3));
+
+        for (int i = 10; i <21 ; i++) {
+            if (i%2==0){
+                System.out.println("wer");
+                return;
+            }else {
+                System.out.println(i);
+            }
+        }
+
+    }
+    public static double multiply(double a , int b){
+        return a*b;*/
+    }
+}
