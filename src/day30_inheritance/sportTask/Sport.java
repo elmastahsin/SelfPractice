7e552629d210c71a77fa1162b56eeffdf14c7d64
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-11-29 19:53:26 +0300

    Inheritance Exercises

--- src/day30_inheritance/sportTask/Sport.java
+++ src/day30_inheritance/sportTask/Sport.java
@@ -1,4 +1,57 @@
 package day30_inheritance.sportTask;
 
 public class Sport {
+    private String name;
+
+
+
+
+
 }
+/*sportTask:
+	1. Create a class named Sport
+			variables:
+				name, numberOfPlayers, numberOfReferre, rules
+
+			Encapsulate all the fields
+					Conditions:
+						1. numberOfPlayers can not be set to zero or negative
+						2. numberOfReferre can not be set to zero or negative
+
+			Add a constructor that can set all the fields
+
+			methods:
+				play()
+				toString()
+
+		2. create the following sub classes of Sport:
+				1. Basketball
+				2. Baseball
+				3. Football
+				4. AmericanFootball
+
+			Add any missing variables or methods
+
+			Note: you can search them on google
+
+
+planetTask:
+	1. create a class called Planet
+			variables:
+					name, mass, radius, surfaceGravity, surfaceArea, volume, population
+
+			Encapsulate all the fields
+
+			Add a constructor that can set all the fields
+
+			methods:
+				toString()
+
+	2. create the following subclasses of Planet
+			1. Earth
+			2. Moon
+			3. Mercury
+			4. Venus
+
+			Note: you can search them on google
+*/
